5f4050ed2f633c666b28546cbad21e33c2fa2472
Samuel Oyenuga <psa8.sa@gmail.com>
2023-11-02 02:04:42 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import "./App.css";
-import Attendance from "./components/attendance";
+import Attendance from "./components/attendance.jsx";
 
 function App() {
   const {
